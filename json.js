d2cb6956e23adcebc77d60b25451f0cfccb2484d
Jason Pan <jasonpan12@users.noreply.github.com>
2018-02-11 09:36:48 -0600

    Update json.js
    Update input processing code to set sub type and sub values dynamically vs having two JWT functions

--- json.js
+++ json.js
@@ -4,21 +4,40 @@ const request = require('request'); // Make HTTP Requests
 const jwt = require('jsonwebtoken'); // Make JWT
 const crypto = require('crypto'); // Crypto stuff, but make random string for JTI
 const yargs = require('yargs'); // read in arguments, i.e. "user" to specify user id
-const _ = require('lodash');
+const _ = require('lodash'); // process data types 'n such
 
 // Read in config.json from developer console
 var boxConfig = JSON.parse(fs.readFileSync('config.json'));
 
 // Read in arguments
 const argv=yargs.argv;
+var command = argv._[0]
+
+// Set box_sub_type based on whether command exists or not
+var subType = (command) => {
+  if (command) { // if command exists, make box_sub_type user
+    return 'user';
+  } else { // if not, assume enterprise
+    return 'enterprise';
+  }
+}
+
+// Same idea, but with sub
+var sub = (command) => {
+  if (command) { // if a command exists, take the id as sub value
+    return _.toString(argv.id);
+  } else { // if not, assume enterprise id from config file
+    return boxConfig.enterpriseID;
+  }
+}
 
 // Create your JWT
-var boxJWTEnt = () => {
+var boxJWT = () => {
   var token = jwt.sign(
     { // Claims aka Payload defined here. Use https://developer.box.com/v2.0/docs/construct-jwt-claim-manually#section-6-constructing-the-claims for reference.
       iss: boxConfig.boxAppSettings.clientID,
-      sub: boxConfig.enterpriseID,
-      box_sub_type: "enterprise",
+      sub: sub(command),
+      box_sub_type: subType(command),
       aud: "https://api.box.com/oauth2/token",
       jti: crypto.randomBytes(20).toString('hex'),
       exp: Math.floor(Date.now() / 1000) + (60)
@@ -35,28 +54,7 @@ var boxJWTEnt = () => {
   return token;
 };
 
-var boxJWTUser = () => {
-  var token = jwt.sign(
-    { // Claims aka Payload defined here. Use https://developer.box.com/v2.0/docs/construct-jwt-claim-manually#section-6-constructing-the-claims for reference.
-      iss: boxConfig.boxAppSettings.clientID,
-      sub: _.toString(argv.id),
-      box_sub_type: argv._[0],
-      aud: "https://api.box.com/oauth2/token",
-      jti: crypto.randomBytes(20).toString('hex'),
-      exp: Math.floor(Date.now() / 1000) + (60)
-    },
-    { // key info goes here
-      key: boxConfig.boxAppSettings.appAuth.privateKey,
-      passphrase: boxConfig.boxAppSettings.appAuth.passphrase
-    },
-    { // kid, alg, and typ made automagically
-      algorithm: "RS256",
-      header: { kid: boxConfig.boxAppSettings.appAuth.publicKeyID },
-    }
-  );
-  return token;
-};
-
+// Make HTTP Request to /token endpoint
 var getAccessToken = (jwt) => {
   var options = {
       method: "POST",
@@ -77,9 +75,5 @@ var getAccessToken = (jwt) => {
 });
 };
 
-// call the functions now.
-if (argv._[0]) { // if any argument exists, i.e. "user", get user token
-  getAccessToken(boxJWTUser());
-} else { // if not, assume enterprise token
-  getAccessToken(boxJWTEnt());
-}
+// call the function now
+getAccessToken(boxJWT());
